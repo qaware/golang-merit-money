f9e89de1153443f6169d6bd7e0231c8ab13ce391
Markus Zimmermann <markus.zimmermann@qaware.de>
2022-07-12 16:04:54 +0200

    add tests for static methods in business

--- business/business_test.go
+++ business/business_test.go
@@ -1,1 +1,42 @@
 package business
+
+import "testing"
+
+func TestNewUuidFromStringError(t *testing.T) {
+	_, err := NewUuidFromString("abc")
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestNewUuidFromStringSuccess(t *testing.T) {
+	_, err := NewUuidFromString("165161")
+	if err != nil {
+		print(err.Error())
+		t.Fail()
+	}
+}
+
+func TestNewQACoinError(t *testing.T) {
+	_, err := NewQaCoin("abc")
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestNewQACoinSuccess(t *testing.T) {
+	_, err := NewQaCoin("123")
+	if err != nil {
+		print(err.Error())
+		t.Fail()
+	}
+}
+
+func TestSliceFind(t *testing.T) {
+	testSlice := []string{"Alex", "Felix", "Markus"}
+	toFind := "Alex"
+	result := sliceFind(testSlice, func(str string) bool { return str == toFind })
+	if result == nil || *result != toFind {
+		t.Fail()
+	}
+}
